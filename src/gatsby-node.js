2d68e49d2910e13dc6e292afaf48ed308c305f36
Andrew Fladmark <andrew.fladmark@gmail.com>
2021-03-04 16:11:45 +0000

    Gatsby 3.0 disallows fs in gatsby-ssr

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,20 +1,19 @@
-import { unlinkSync } from 'fs';
 import LoadablePlugin from '@loadable/webpack-plugin';
-import { statsPath } from './constants';
+import { statsFilename } from './constants';
 
 export const onCreateWebpackConfig = ({ actions, stage }) => {
   if (stage === "build-javascript" || stage === "develop") {
     actions.setWebpackConfig({
-      plugins: [new LoadablePlugin({ filename: statsPath, writeToDisk: true })]
+      plugins: [
+        new LoadablePlugin({ 
+          filename: stage === "develop" ? `public/${statsFilename}` : statsFilename, 
+          writeToDisk: true
+        })
+      ],
     });
   }
 };
 
 export const onCreateBabelConfig = ({ actions }) => {
-  actions.setBabelPlugin({ name: '@loadable/babel-plugin' });
-};
-
-export const onPostBuild = () => {
-  // Clean after ourselves
-  unlinkSync(statsPath);
+  actions.setBabelPlugin({ name: "@loadable/babel-plugin" });
 };
