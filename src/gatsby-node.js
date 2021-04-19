c26d2268630b7e8e5a92c22099c4d0fb85054ae0
drskigen <patricskigen@gmail.com>
2021-04-18 21:43:05 -0400

    Stats file does not exist in develop

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -3,11 +3,11 @@ import LoadablePlugin from '@loadable/webpack-plugin';
 import { statsFilename, statsPath } from './constants';
 
 export const onCreateWebpackConfig = ({ actions, stage }) => {
-  if (stage === "build-javascript" || stage === "develop") {
+  if (stage === 'build-javascript' || stage === 'develop') {
     actions.setWebpackConfig({
       plugins: [
         new LoadablePlugin({
-          filename: stage === "develop" ? `public/${statsFilename}` : statsFilename,
+          filename: stage === 'develop' ? `public/${statsFilename}` : statsFilename,
           writeToDisk: true,
         }),
       ],
